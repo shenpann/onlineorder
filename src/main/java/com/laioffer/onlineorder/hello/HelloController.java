eea6ce48a495a3fe60e9817e971660d5570a4b18
Jerry Zhang <xinhao926@gmail.com>
2026-08-14 18:17:44 -0400

    Online order app

---
+++ src/main/java/com/laioffer/onlineorder/hello/HelloController.java
@@ -0,0 +1,27 @@
+package com.laioffer.onlineorder.hello;
+
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public Person sayHello(@RequestParam(required = false, defaultValue = "Guest") String name) {
+        return new Person(
+                name,
+                "laioffer",
+                new Address("123 Happy Street", "San Francisco", "California", null),
+                new Book("Clean Code", "John Smith")
+        );
+    }
+}
+
+
+
